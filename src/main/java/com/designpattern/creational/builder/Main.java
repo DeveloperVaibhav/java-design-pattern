d080fa3bfe8a79e3b79457a58bcfd6076d85d727
DeveloperVaibhav <vaibhavchafle09@gmail.com>
2026-02-22 00:54:12 +0530

    added comments in main class

--- src/main/java/com/designpattern/creational/builder/Main.java
+++ src/main/java/com/designpattern/creational/builder/Main.java
@@ -3,12 +3,14 @@ package com.designpattern.creational.builder;
 public class Main {
     public static void main(String[] args) {
 
+        // Create meal with required burger and optional drink
         Meal meal1 = new Meal.Builder(Burger.HAMBURGER)
                 .drink(Drink.COKE)
                 .build();
 
         System.out.println(meal1);
 
+        // Create meal with required burger, optional drink and fries
         Meal meal2 = new Meal.Builder(Burger.CHEESE_BURGER)
                 .drink(Drink.ICED_TEA)
                 .fries(Fries.FRENCH_FRIES)
